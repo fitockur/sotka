5d9f4c710034f32ef72ae91557f48a7ed5c81444
Nikita Kozyrev <kozyrevnm@gmail.com>
2020-04-27 12:51:49 +0300

    add custom1 tets

---
+++ test_plus_task.py
@@ -0,0 +1,32 @@
+from LiteratureTest import LiteratureTest, IncorrectTestNum
+from pyperclip import copy
+
+if __name__ == "__main__":
+    while True:
+        try:
+            res = []
+            test_answers = 'test_plus_9.txt'
+            hw_num = 11
+            task_numbers = ('9',)
+            which = input('\nВыбери уровень сложности: \n\t1) Легкий\n\t2) Сложный\n>> ')
+            if which == '1':
+                res.append(LiteratureTest(test_type='test', test_answers=test_answers, hw_num=hw_num))
+            elif which == '2':
+                res.append(LiteratureTest(test_type='test', test_answers=test_answers, hw_num=hw_num))
+                res.append(LiteratureTest(test_type='task', task_numbers=task_numbers, hw_num=hw_num))
+            else:
+                raise IncorrectTestNum
+            buff = ''
+            for i, lit in enumerate(res):
+                lit.start()
+                if i:
+                    buff += '\n'.join(lit.text_body.split('\n')[2:])
+                else:
+                    buff += lit.text_body
+            copy(buff)
+        except KeyboardInterrupt:
+            print('\nЗавершение работы...')
+            break
+        except (IncorrectTestNum, EOFError):
+            print('\nТы ввела неверный вариант задания! Попробуй еще раз!')
+            continue
